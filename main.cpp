f1a4562317875fbafc9a0b48430c7aec782dbdbe
Alskito <zamudioalexander2@gmail.com>
2025-12-04 18:49:37 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
  * de distintos vehiculos deppendiendo de sus caracteristicas
  */
 
+ //Bibliotecas
 #include<iostream>
 #include<string>
 #include "Concesionaria.h"
@@ -17,104 +18,104 @@
 
 
 int main(){
-    Concesionaria Concesionaria_Benito_Juarez_IDK; // Nombre de la concesionaria
-    int opcion = 0; 
+    Concesionaria Concesionaria_Benito_Juarez_IDK; // objeto concesionaria
+    int opcion = 0;
     // Varibles temporales para los cin
-    std::string t_marca; 
+    std::string t_marca;
     std::string t_modelo;
     int t_precio;
     int t_year;
     int t_km;
     int t_extra;
 
-    do {
+    do { // do while para forzar la entrada al menu
         // Menu para que todo se vea bonito
-        std::cout << "\n=================================================" 
+        std::cout << "\n================================================="
         << std::endl;
-        std::cout << "      APLICACION PARA GESTIONAR PRECIOS :d     " 
+        std::cout << "      APLICACION PARA GESTIONAR PRECIOS :d     "
         << std::endl;
-        std::cout << "=================================================" 
+        std::cout << "================================================="
         << std::endl;
         std::cout << "1. Agregar MOTO" << std::endl;
         std::cout << "2. Agregar COCHE" << std::endl;
         std::cout << "3. Agregar CAMIONETA" << std::endl;
         std::cout << "4. Mostrar Precios de Venta" << std::endl;
         std::cout << "5. Salir" << std::endl;
-        std::cout << "-----------------------------------------" 
+        std::cout << "-----------------------------------------"
         << std::endl;
         std::cout << "Selecciona una opcion(1 al 5): ";  std::cin >> opcion;
 
 
         switch(opcion) {
-            case 1: { 
+            case 1: {
                 // Se agrega una moto
                 std::cout << "\n[NUEVA MOTO]" << std::endl;
-                std::cout << "Marca(NO USES ESPACIOS): ";      
+                std::cout << "Marca(NO USES ESPACIOS): ";
                 std::cin >> t_marca;
-                std::cout << "Modelo(NO USES ESPACIOS): ";     
+                std::cout << "Modelo(NO USES ESPACIOS): ";
                 std::cin >> t_modelo;
-                std::cout << "Precio Base(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Precio Base(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_precio;
-                std::cout << "Year(SOLO USA NUMEROS ENTEROS): ";        
+                std::cout << "Year(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_year;
-                std::cout << "Kilometraje(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Kilometraje(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_km;
-                std::cout << "Cilindraje (CC),(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Cilindraje (CC),(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_extra;
 
                 Moto mo(t_marca, t_modelo, t_precio, t_year, t_km, t_extra);
-            
+
                 Concesionaria_Benito_Juarez_IDK.agregar_moto(mo);
                 std::cout << ">> Moto agregada" << std::endl;
                 break;
             }
 
-            case 2: { 
+            case 2: {
                 // Se agrega un coche
                 std::cout << "\n[NUEVO COCHE]" << std::endl;
-                std::cout << "Marca(NO USES ESPACIOS): ";      
+                std::cout << "Marca(NO USES ESPACIOS): ";
                 std::cin >> t_marca;
-                std::cout << "Modelo(NO USES ESPACIOS): ";     
+                std::cout << "Modelo(NO USES ESPACIOS): ";
                 std::cin >> t_modelo;
-                std::cout << "Precio Base(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Precio Base(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_precio;
-                std::cout << "Year(SOLO USA NUMEROS ENTEROS): ";        
+                std::cout << "Year(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_year;
-                std::cout << "Kilometraje(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Kilometraje(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_km;
-                std::cout << "Numero de Puertas(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Numero de Puertas(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_extra;
-                
+
                 Coche co(t_marca, t_modelo, t_precio, t_year, t_km, t_extra);
-           
-                
+
+
                 Concesionaria_Benito_Juarez_IDK.agregar_coche(co);
                 std::cout << ">> Coche agregado" << std::endl;
                 break;
             }
 
-            case 3: { 
+            case 3: {
                 // Se agrega un camioneta
                 std::cout << "\n[NUEVA CAMIONETA]" << std::endl;
-                std::cout << "Marca(NO USES ESPACIOS): ";     
+                std::cout << "Marca(NO USES ESPACIOS): ";
                 std::cin >> t_marca;
-                std::cout << "Modelo(NO USES ESPACIOS): ";     
+                std::cout << "Modelo(NO USES ESPACIOS): ";
                 std::cin >> t_modelo;
-                std::cout << "Precio Base(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Precio Base(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_precio;
-                std::cout << "Year(SOLO USA NUMEROS ENTEROS): ";        
+                std::cout << "Year(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_year;
-                std::cout << "Kilometraje(SOLO USA NUMEROS ENTEROS): "; 
+                std::cout << "Kilometraje(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_km;
                 std::cout << "Capacidad de Carga (L),"
-                    <<"(SOLO USA NUMEROS ENTEROS): "; 
+                    <<"(SOLO USA NUMEROS ENTEROS): ";
                 std::cin >> t_extra;
 
-                Camioneta ca(t_marca, t_modelo, 
+                Camioneta ca(t_marca, t_modelo,
                     t_precio, t_year, t_km, t_extra);
 
-               
-                
+
+
                 Concesionaria_Benito_Juarez_IDK.agregar_camioneta(ca);
                 std::cout << ">> Camioneta agregada" << std::endl;
                 break;
@@ -128,21 +129,21 @@ int main(){
 
             case 5:{
                 // Mensajito de despedida :D
-                std::cout << "MENSAJE DE DESPEDIDA GENERICO (•　ω　•) " 
+                std::cout << "MENSAJE DE DESPEDIDA GENERICO (•　ω　•) "
                 << std::endl;
                 break;
             }
             default:
-                std::cout << "Selecciona uno de los" 
-                << "numeros del 1 al 5 (⌣̀_⌣́) )" 
+                std::cout << "Selecciona uno de los"
+                << "numeros del 1 al 5 (⌣̀_⌣́) )"
                 << std::endl;
-            
-        }
 
+        }
+//Ciclo para que el sistema siga corriendo mientras no elija la opcion salir.
     } while(opcion != 5);
 
 
-    
+
 
     return 0;
 }
